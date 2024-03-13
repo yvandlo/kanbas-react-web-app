99e59c0086be966ac0d670ded6250b6852b125f9
Yvandlo <44481260+yvandlo@users.noreply.github.com>
2024-03-13 18:21:57 -0400

    a4 start

--- src/Labs/a4/index.tsx
+++ src/Labs/a4/index.tsx
@@ -1,8 +1,39 @@
-function Assignment4() {
-  return (
-    <div className="container">
-      <h1>Assignment 4</h1>
-    </div>
-  );
+import React from "react";
+import ClickEvent from "./ClickEvent";
+import PassingDataOnEvent from "./PassingDataOnEvent";
+import PassingFunctions from "./PassingFunctions";
+import EventObject from "./EventObject";
+import Counter from "./Counter";
+import BooleanStateVariables from "./BooleanStateVariables";
+import StringStateVariables from "./StringStateVariables";
+import DataStateVariables from "./DataStateVariables";
+import ObjectStateVariable from "./ObjectStateVariable";
+import ArrayStateVariable from "./ArrayStateVariable";
+import ParentStateComponent from "./ParentStateComponent";
+import ReduxExamples from "./ReduxExamples";
+
+function sayHello() {
+  alert("Hello");
 }
+
+
+const Assignment4 = () => {
+  return(
+    <>
+      <h1>Assignment 4</h1>
+      <ReduxExamples/>
+      <ParentStateComponent />
+      <ArrayStateVariable />
+      <ObjectStateVariable />
+      <DataStateVariables />
+      <StringStateVariables />
+      <BooleanStateVariables />
+      <Counter />
+      <EventObject />
+      <PassingFunctions theFunction={sayHello} />
+      <PassingDataOnEvent />
+      <ClickEvent />
+    </>
+  );
+};
 export default Assignment4;
