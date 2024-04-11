ff9a63346bcc348eeb4f7590b2a8aa849d8a4f94
Yvandlo <44481260+yvandlo@users.noreply.github.com>
2024-04-11 13:27:10 -0400

    fixing link

--- src/Kanbas/Courses/index.tsx
+++ src/Kanbas/Courses/index.tsx
@@ -8,12 +8,13 @@ import Home from "./Home";
 import Assignments from "./Assignments";
 import { links } from "../Database"
 import { FaGreaterThan } from "react-icons/fa";
-
+const API_BASE = process.env.REACT_APP_API_BASE;
 
 function Courses() {
   const { courseId, section } = useParams();
   const { pathname } = useLocation();
-  const COURSES_API = "http://localhost:4000/api/courses";
+  //const COURSES_API = "http://localhost:4000/api/courses";
+  const COURSES_API = `${API_BASE}/api/courses`;
   const [course, setCourse] = useState<any>({ _id: "" });
   const findCourseById = async (courseId?: string) => {
     const response = await axios.get(
